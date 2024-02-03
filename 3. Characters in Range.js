14761ac5ae3388a0ee7248cd2f51f807637c812b
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-03 17:31:33 +0200

    Add files via upload

---
+++ 3. Characters in Range.js
@@ -0,0 +1,25 @@
+function charactersInRange(str1, str2) {
+    let char1 = str1.charCodeAt(0);
+    let char2 = str2.charCodeAt(0);
+    let res = "";
+    if (char1 <= char2) {
+        for (let i = char1 + 1; i < char2; i++) {
+            if (i>=0 && i<=127){
+            let currentChar = String.fromCharCode(i);
+            res += currentChar + " ";
+        }
+    }
+    } else {
+        for (let i = char2+1; i < char1; i++) {
+            if (i>=0 && i<=127){
+                
+            let currentChar = String.fromCharCode(i);
+            res += currentChar + " ";
+        }
+    }
+    }
+    console.log(res);
+}
+//charactersInRange('a','d');
+charactersInRange('#',':');
+//charactersInRange('C', '#');
